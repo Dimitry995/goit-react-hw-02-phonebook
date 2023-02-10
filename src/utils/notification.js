c5eb972e00845e7b06afe939162231cff07b2d13
Dimitry995 <98602983+Dimitry995@users.noreply.github.com>
2023-02-10 08:42:31 +0000

    Deploying to gh-pages from @ Dimitry995/goit-react-hw-02-phonebook@e88af6162b9da833090741cd8df49912f862535c 🚀

--- src/utils/notification.js
+++
@@ -1,8 +0,0 @@
-import { Notify } from 'notiflix';
-
-export const Notification = addedName =>
-  Notify.warning(`${addedName} is already in contacts`, {
-    timeout: 4000,
-    fontSize: '22px',
-    position: 'center-center',
-  });
